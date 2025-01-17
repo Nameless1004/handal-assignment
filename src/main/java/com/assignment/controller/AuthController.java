4f246e8ee1c37f2f2cb510f207e8c8fb2ae4eefb
Nameless1004 <jjho1029@gmail.com>
2025-01-17 21:41:02 +0900

    [feat] Auth 컨트롤러 스웨거 문서화 작업 * 스키마 추가 * ApiResponse 추가

--- src/main/java/com/assignment/controller/AuthController.java
+++ src/main/java/com/assignment/controller/AuthController.java
@@ -6,11 +6,16 @@ import com.assignment.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,12 +28,16 @@ public class AuthController {
 
     @Operation(summary = "회원가입", description = "회원가입 API 입니다.")
     @PostMapping("/signup")
+    @ApiResponse(responseCode = "201", description = "성공", content = {
+            @Content(schema = @Schema(implementation = AuthResponseDto.Signup.class))})
     public ResponseEntity<AuthResponseDto.Signup> signup(@Valid @RequestBody AuthRequestDto.Signup request){
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(request));
     }
 
     @Operation(summary = "로그인", description = "로그인 API 입니다.")
     @PostMapping("/signin")
+    @ApiResponse(responseCode = "201", description = "성공", content = {
+            @Content(schema = @Schema(implementation = AuthResponseDto.Signin.class))})
     public ResponseEntity<AuthResponseDto.Signin> signin(@Valid @RequestBody AuthRequestDto.Signin request){
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signin(request));
     }
--- src/main/java/com/assignment/dto/AuthResponseDto.java
+++ src/main/java/com/assignment/dto/AuthResponseDto.java
@@ -1,6 +1,7 @@
 package com.assignment.dto;
 
 import com.assignment.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
@@ -8,11 +9,21 @@ import java.util.Collection;
 import java.util.List;
 
 public sealed interface AuthResponseDto permits AuthResponseDto.Signup, AuthResponseDto.Signin {
-    record Signup(String username, String nickname, Collection<? extends GrantedAuthority> authorities) implements AuthResponseDto {
+    record Signup(
+            @Schema(description = "유저네임 입니다.", example = "jaeho")
+            String username,
+            @Schema(description = "닉네임 입니다.", example = "용사")
+            String nickname,
+            @Schema(description = "권한 목록입니다.", example = "{\"authority\": \"ROLE_USER\"}")
+            Collection<? extends GrantedAuthority> authorities) implements AuthResponseDto {
         public Signup(User user) {
             this(user.getUsername(), user.getNickname(), List.of(new SimpleGrantedAuthority(user.getRole())));
         }
     }
 
-    record Signin(String accessToken, String refreshToken) implements AuthResponseDto { }
+    record Signin(
+            @Schema(description = "액세스 토큰입니다.", example = "eyJhbGciOiJIUzI1NiJ9")
+            String accessToken,
+            @Schema(description = "액세스 토큰입니다.", example = "eyJhbGciOiJIUzI1NiJ9")
+            String refreshToken) implements AuthResponseDto { }
 }
