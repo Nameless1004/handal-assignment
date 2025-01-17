65a9499cbf0ad6ad0389e87bf699f93d92a29124
Nameless1004 <jjho1029@gmail.com>
2025-01-17 13:37:10 +0900

    [feat] 토큰 검증 필터 추가

---
+++ src/main/java/com/assignment/filter/TokenFilter.java
@@ -0,0 +1,28 @@
+package com.assignment.filter;
+
+import com.assignment.components.TokenUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "Auth")
+@RequiredArgsConstructor
+public class TokenFilter extends OncePerRequestFilter {
+
+    private final TokenUtils tokenUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+        log.info("[{}] {}", method, uri);
+        filterChain.doFilter(request, response);
+    }
+}
