d4c2713e024fdf2e10dfb366b16448c099e61b9c
Nameless1004 <jjho1029@gmail.com>
2025-01-17 22:50:27 +0900

    [fix] 스웨거 스키마 버그 수정

--- src/main/java/com/assignment/domain/auth/controller/AuthController.java
+++ src/main/java/com/assignment/domain/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.assignment.domain.auth.dto.AuthResponseDto;
 import com.assignment.domain.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,22 +26,48 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Operation(summary = "회원가입",
+    @Operation(
+            summary = "회원가입",
             description = "회원가입 API 입니다.",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(schema = @Schema(implementation = AuthRequestDto.Signup.class))))
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(implementation = AuthRequestDto.Signup.class),
+                            examples = @ExampleObject(
+                                    name = "회원가입 요청 예시",
+                                    value = "{ \"username\": \"jaeho\", \"password\": \"abcd1234\", \"nickname\": \"용사\" }"
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "성공", content = {
+                            @Content(schema = @Schema(implementation = AuthResponseDto.Signup.class))
+                    })
+            }
+    )
     @PostMapping("/signup")
-    @ApiResponse(responseCode = "201", description = "성공", content = {
-            @Content(schema = @Schema(implementation = AuthResponseDto.Signup.class))})
     public ResponseEntity<AuthResponseDto.Signup> signup(@Valid @RequestBody AuthRequestDto.Signup request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(request));
     }
 
-    @Operation(summary = "로그인",
+    @Operation(
+            summary = "로그인",
             description = "로그인 API 입니다.",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(schema = @Schema(implementation = AuthRequestDto.Signin.class))))
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            schema = @Schema(implementation = AuthRequestDto.Signin.class),
+                            examples = @ExampleObject(
+                                    name = "로그인 요청 예시",
+                                    value = "{ \"username\": \"jaeho\", \"password\": \"abcd1234\" }"
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "성공", content = {
+                            @Content(schema = @Schema(implementation = AuthResponseDto.Signin.class))
+                    })
+            }
+    )
     @PostMapping("/signin")
-    @ApiResponse(responseCode = "201", description = "성공", content = {
-            @Content(schema = @Schema(implementation = AuthResponseDto.Signin.class))})
     public ResponseEntity<AuthResponseDto.Signin> signin(@Valid @RequestBody AuthRequestDto.Signin request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signin(request));
     }
